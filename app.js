c36d7628ca0d148e6478b0a8323a498578589743
Sina Sangiji <sina.sangiji@gmail.com>
2022-07-07 21:41:45 +0430

    Added so much Animations

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
+'use strict';
+
+const listItemEls = document.querySelectorAll(".list__item")
+const specialtiesEls = document.querySelectorAll(".specialties")
+const conditionsEls = document.querySelectorAll(".conditions")
+const proceduresEls = document.querySelectorAll(".procedures")
+
 // Removing Search Tags
 function deleteitem(id) {
   let number = id.slice(-1);
   let itemId = "list_" + number;
   document.getElementById(itemId).outerHTML = "";
+}
+
+const specialties = () => {
+  for (const listItemEl of listItemEls) {
+    listItemEl.style.display = "none";
+    for (const item of specialtiesEls) {
+      item.style.display = "block";
+    }
+  }
+}
+const conditions = () => {
+  for (const listItemEl of listItemEls) {
+    listItemEl.style.display = "none";
+    for (const item of conditionsEls) {
+      item.style.display = "block";
+    }
+  }
+}
+const procedures = () => {
+  for (const listItemEl of listItemEls) {
+    listItemEl.style.display = "none";
+    for (const item of proceduresEls) {
+      item.style.display = "block";
+    }
+  }
 }
